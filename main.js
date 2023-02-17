ed359081d340946e70967de3969305c5b3b29426
Manik <manik@pixelconception.com>
2023-02-17 18:33:08 +0530

    pushing new lecture

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 class User {
   constructor(name, email) {
     this.name = name;
@@ -20,7 +21,29 @@ class User {
 }
 
 const user = new User('John', 'john@email.com');
-const user2 = new User('Mark', 'mark@email.com');
 
 console.log(user);
-user2.addPoint();
+*/
+
+function User(name, email) {
+  this.name = name;
+  this.email = email;
+  this.points = 0;
+
+  this.login = () => {
+    console.log(this.name, 'Has logged in');
+  };
+
+  this.logout = () => {
+    console.log(this.name, 'Has logged out');
+  };
+
+  this.addPoint = () => {
+    this.points++;
+    console.log('total points', this.points);
+  };
+}
+
+const user = new User('John', 'john@email.com');
+
+console.log(user);
