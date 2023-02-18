335d91335d3678eb6661f1143248db142c320ced
Manik <manik@pixelconception.com>
2023-02-18 13:01:56 +0530

    pushing new lecture

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
-let x = 'y'; // string and not litral value Y
-const b = 'john';
-let y = 78;
-var c = false;
-let d = null;
-const z = {
-  x: 14,
-};
+const numbers = {
+  x: 10,
+  y: {
+    z: 20,
+  },
+} as const;
 
-enum Roles {
-  admin = 'admin',
-  author = 'author',
-}
+let firstNameField = document.querySelector(
+  '#firstName'
+)! as HTMLInputElement;
 
-const user = Roles.admin;
+firstNameField.value;
