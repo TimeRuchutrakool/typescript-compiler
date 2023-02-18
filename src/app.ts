0ff0be495a34d20f375982e7df9cfc7af1730132
Manik <manik@pixelconception.com>
2023-02-18 13:07:11 +0530

    pushing new lecture

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,39 @@
-type Weekdays = 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri';
+let firstName = 'Mark';
 
-type Day = Weekdays | 'Sat' | 'Sun';
+console.log(typeof firstName);
 
-function nextDayForAWeekDay(weekday: Weekdays): Day {
-  switch (weekday) {
-    case 'Mon':
-      return 'Tue';
-    case 'Tue':
-      return 'Wed';
-    case 'Wed':
-      return 'Thu';
-    case 'Thu':
-      return 'Fri';
-    case 'Fri':
-      return 'Sat';
+type Cat = {
+  type: 'cat';
+  purrs: boolean;
+};
+
+type Dog = {
+  type: 'dog';
+  barks: boolean;
+};
+
+type Animal = Cat | Dog;
+
+let cat: Animal = {
+  type: 'cat',
+  purrs: true,
+};
+
+let dog: Animal = {
+  type: 'dog',
+  barks: true,
+};
+
+function animalReaction(animal: Animal) {
+  switch (animal.type) {
+    case 'cat':
+      console.log('The animal is a cat and it purrs');
+      break;
+    case 'dog':
+      console.log('The Animal is a dog and it barks');
+      break;
   }
 }
+
+animalReaction(dog);
+animalReaction(cat);
