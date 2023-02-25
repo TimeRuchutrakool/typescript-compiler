3dc9757c154160f017997e27ed96eccdbc1d007d
Manik <manik@pixelconception.com>
2023-02-25 12:59:28 +0530

    pushing new lecture

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+let greeting = 32;
+let firstName: typeof greeting;
+
+const user = {
+  name: "John",
+  age: 32,
+};
+
+let tom: typeof user;
+
+tom = {
+  name: "Tom",
+  age: 22,
+  email: "tom@email.com",
+};
