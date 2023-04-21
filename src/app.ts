3357235dae23c3f3a27fdfb0640bf4b6b9ad69ba
Manik <manik@pixelconception.com>
2023-04-21 10:06:59 +0530

    pushing code for new lecture

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+// ===
+// !==
+// ==
+// and !=
+
+function convertStrings(a: string | number, b: string | boolean) {
+  if (a === b) {
+    console.log(a.toUpperCase());
+    console.log(a.toLowerCase());
+  }
+}
