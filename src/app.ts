1b48b0e4307fb0899cd0169915decf093bc290cb
Manik <manik@pixelconception.com>
2023-05-04 16:19:21 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
-// ===
-// !==
-// ==
-// and !=
+type Circle = {
+  kind: "circle";
+  radius: number;
+};
+
+type Square = {
+  kind: "square";
+  sideLength: number;
+};
+
+type Shape = Circle | Square;
+
+function getArea(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  } else {
+    return shape.sideLength ** 2;
+  }
+}
