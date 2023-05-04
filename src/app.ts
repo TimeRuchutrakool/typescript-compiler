1426ef82fadca9825a7589b7c1fed7e4e2b221ad
Manik <manik@pixelconception.com>
2023-05-04 16:01:55 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,3 @@
 // !==
 // ==
 // and !=
-
-function convertStrings(a: string | number, b: string | boolean) {
-  if (a === b) {
-    console.log(a.toUpperCase());
-    console.log(a.toLowerCase());
-  }
-}
