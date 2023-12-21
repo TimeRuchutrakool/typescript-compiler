915adab6c116f0ee781fd028d10a1df7342c18f5
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-21 23:08:47 +0700

    1

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+
+// เป็นที่รู้กันว่า every property ใน obj มันจะมี object associated with it ซึ่งเรียกว่า property descriptor
+
+
 // value -> The value of the property
 // writable (boolean) -> whether this property in question is writable or not
-// enumerable (boolean)  -> whether you can enumerate or loop through this property or not
+// enumerable (boolean)  -> whether you can enumerate or loop through this property or not //? สามารถ loop throughได้มั้ย
 // configurable (boolean) -> The configurable property tells whether the user has permission
 //								           to change property descriptor such as to change the value of
-//                           writable and enumerable settings.
+//                           writable and enumerable settings. //?ถ้าเป็น true คือ property สามารถถูก change ได้
 
-const book = {
+const book = { 
   title: 'the title',
   pages: 300,
   author: 'John',
