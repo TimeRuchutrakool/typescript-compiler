d624fcc4f709ccdb4a0609e41885d182dd51aa17
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-21 17:18:30 +0700

    1

--- main.js
+++ main.js
@@ -31,19 +31,21 @@ function User(name, email) {
   this.points = 0;
 
   this.login = () => {
-    console.log(this.name, 'Has logged in');
+    console.log(this.name, "Has logged in");
   };
 
   this.logout = () => {
-    console.log(this.name, 'Has logged out');
+    console.log(this.name, "Has logged out");
   };
 
   this.addPoint = () => {
     this.points++;
-    console.log('total points', this.points);
+    console.log("total points", this.points);
   };
 }
 
-const user = new User('John', 'john@email.com');
+// ! เมื่อใช้ new keyword JS มันจะ treat this function as a constructor function
+// หลังจากนั้นสิ่งที่มันทำคือ create empty obj //this ของ function จะ refer ไปหา {} ที่สร้างขึ้นตะกี้ แล้ว add all properties 1 by 1
+const user = new User("John", "john@email.com");
 
 console.log(user);
