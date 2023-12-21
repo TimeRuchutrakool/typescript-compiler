c6205dc54ff81eb6df8d5da15f2917277a5fe853
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-21 23:39:03 +0700

    1

--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,26 @@
  * function that will be called at runtime with
  * information about the decorated declaration.
  * */
+
+// to modify their behavior (class declaration/method/accessor/property/parameter) or add metadata to them
+
+@LogClass
+class myClass {
+  constructor(public name: string) { }
+
+  @RunLog
+  run() {
+    console.log('run')
+  }
+}
+
+
+function LogClass(constructor: Function) {
+  console.log(constructor)
+}
+
+function RunLog(a: any, b: any, c: any) {
+  console.log(a)
+  console.log(b)
+  console.log(c)
+}
