7e9716ee132bd7e5741c3d1c8cdc3d32f70a8d3b
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-22 15:06:59 +0700

    1

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,8 @@ interface Dog extends Animal {
   woof: () => void;
 }
 
-type Example = Dog extends Animal ? string : number;
+// ? เราสามารถ check ว่า typeนึงมัน extends the other type แล้วก็ perform actions based on the condition ที่เป็น true
+type Example = Dog extends Animal ? string : number; //ถ้า dog มัน extends animal Example จะเป็น string
 
 // SomeType extends OtherType ? TrueType : FalseType;
 type isString<T> = T extends string ? true : false;
