fa59e6184fe705efbdd352b88e62c63eeebe5919
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-22 12:01:11 +0700

    1

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 let x = 'y'; // string and not litral value Y
-const b = 'john';
-let y = 78;
+const b = 'john'; // TS มันมองว่าเป็น const ไม่เปลี่ยนค่าอยู่แล้วมันเลยให้เป็น literal type
+let y = 78; //มันยังสามารถเป็น 60 20 10 ได้อยู่ in the future มันเลยให้เป็น number
 var c = false;
 let d = null;
 const z = {
