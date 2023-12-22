cbbd301586b1470fd81021184d46c7e86e00dd26
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-22 15:32:31 +0700

    1

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,52 @@
-interface WritePermissions {
-  write: true;
+// interface WritePermissions {
+//   write: true;
+// }
+
+// interface ReadPermissions {
+//   write: false;
+// }
+
+// interface User {
+//   id: number;
+//   name: string;
+//   email: string;
+// }
+
+// interface Admin extends User, WritePermissions {}
+// interface Author extends User, WritePermissions {}
+// interface Reader extends User, ReadPermissions {}
+
+// type FilterWritable<T> = T extends { write: true } ? true : false;
+
+// type Writable = FilterWritable<Reader>;
+
+interface WritePermissions{
+  write:true
 }
 
-interface ReadPermissions {
-  write: false;
+interface ReadPermissions{
+  write:false
 }
 
-interface User {
+interface User{
   id: number;
   name: string;
   email: string;
 }
 
-interface Admin extends User, WritePermissions {}
-interface Author extends User, WritePermissions {}
-interface Reader extends User, ReadPermissions {}
+// เราจะเรียกใช้ filtered writable ซึ่งเราจะ pass in one of those interface to a parent interface as a generic แล้วปล่อยให้ interface decide ว่าเป็น admin user หรือ author
 
-type FilterWritable<T> = T extends { write: true } ? true : false;
+interface Admin extends User, WritePermissions{
+  
+}
+interface Author extends User, WritePermissions{
+  
+}
+interface Reader extends User, ReadPermissions{
+  
+}
 
-type Writable = FilterWritable<Reader>;
+type FilterWritable<T> = T extends { write: true } ? true : false
+
+// type Writable = FilterWritable<Admin>
+type Writable = FilterWritable<Reader>
