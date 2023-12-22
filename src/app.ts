dd3ac8bb87d38076fe832fd7c5d7573ed4182aec
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-22 11:14:47 +0700

    1

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,34 @@ interface MapLocation {
   long: number;
 }
 
-function AddLocation(lat: number, long: number) {
-  return <T extends { new (...args: any[]): {} }>(
+// function AddLocationn(classConstructor: FunctionConstructor) {
+//   return class extends classConstructor{
+//     constructor(){super()}
+//   }
+// }//แปลงให้เป็น factory
+
+//!  |
+//!  |
+//!  V
+
+// function AddLocationnn(lat:number,lng:number) { 
+//   return function(classConstructor: FunctionConstructor) {
+//   return class extends classConstructor{
+//     constructor(){super()}
+//   }
+// }
+// }
+
+
+//!  |
+//!  |
+//!  V
+
+function AddLocation(lat: number, long: number) { //จุดประสงค์มันคือ class ไหนที่ถูก decorate ด้วย ตัวนี้จะมี property mapLocation เพิ่ม
+  return <T extends { new (...args: any[]): {} }>( //new keyword จะบอก ว่ามันจะ return object //new (...args: any[]): {} คือ function ที่มี rest param args
     classConstructor: T
   ) => {
-    return class extends classConstructor {
+    return class extends classConstructor { //return class ที่มัน extends constructor ตัวที่ไปเกาะ
       public mapLocation: MapLocation;
       constructor(...args: any[]) {
         super(...args);
@@ -17,6 +40,7 @@ function AddLocation(lat: number, long: number) {
   };
 }
 
+// class decorator มัน สามารถ alter a constructor function และ return a new constructor function จากใน decorator มันเองได้
 @AddLocation(1.234, 1.876)
 class Person {
   constructor(public name: string, public age: number) {}
