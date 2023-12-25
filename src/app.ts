e182d092e585a4baf5a7df4ede70c67d2de4bfea
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-25 09:51:41 +0700

    1

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
-// Mapped Types
+//* Utility type คือ predefined generic type -> เราสามารถ perform different types of transformation in TS -> Type พวกนี้มันเอาไว้ manipulate และก็ create new types
+
+//! Utility types can be considered as type function that take one or more types as an input and produce a new type as an output
+
+
+// Mapped Types -> ช่วยให้เรา create a new type based on existing type ด้วยการ apply mapping function to each property of the original type
 // Partial Type Implementation
 type Partial<T> = {
   [P in keyof T]?: T[P];
