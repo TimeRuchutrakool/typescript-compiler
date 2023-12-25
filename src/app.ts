9a2919a6501aa9cf92e92a74f658ab3d10762778
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-25 10:13:43 +0700

    1

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+// !จะใช้สำหรับ combine two different types together into one -> มัน allow you to create an object type whose property are of a specific type 
+// ก็คือ รับ 2 different type and converting one of the type to object keys and the another type t object value
+
 // Record<Keys, Type>
 
 type Roles = "author" | "editor" | "researcher";
@@ -11,7 +14,8 @@ interface User {
 interface Article {
   title: string;
   content: string;
-  contributors: Record<Roles, User>;
+  // contrbutors คือ derivation ของ Roles กับ User
+  contributors: Record<Roles, User>; //! มัน take each value of Roles เป็น key แล้ว assign value เป็น interface User
 }
 
 const article: Article = {
