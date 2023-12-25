bec25bea9150db74bb2d1e78d330bbcbb8703474
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-25 10:23:38 +0700

    1

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-//  Pick<Type, Keys>;
+// ! ช่วยให้เรา create a new type by picking some of the subsets or properties of an existing type
+
+//  Pick<Type, Keys>; //ตัวแรกเป็น original type หรือ properties of that particular type //ส่วน keys is an array of props ที่เรา want to pick up from the original type
 
 interface Person {
   name: string;
@@ -6,7 +8,8 @@ interface Person {
   address: string;
 }
 
-type NameAndAge = Pick<Person, "name" | "age">;
+type NameAndAge = Pick<Person, "name" | "age">; //pick name and age and ignore address prop
+type ReadonlyNameAndAge =Readonly<Pick<Person, "name" | "age">>
 
 const person: NameAndAge = {
   name: "John",
